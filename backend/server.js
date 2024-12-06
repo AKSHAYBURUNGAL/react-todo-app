c7d06154303614fa3ba9b068e4f6a82b2ea502eb
Akshay Burungale <burungaleakshay88@gmail.com>
2024-12-06 09:22:46 +0530

    Initial commit of To-Do List React App

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/todo-app', {
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/todo-app'; // Use env variable for Docker
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
 });
 
 // Define Schema and Model
@@ -50,6 +55,7 @@ app.delete('/todos/:id', async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
